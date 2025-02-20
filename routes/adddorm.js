40af66233ee65d2e51412e33470c47243457b296
insisapim <66070322@kmitl.ac.th>
2025-02-20 22:13:16 +0700

    adddorm, uploads directory
    fileupload insert dormitory data

--- routes/adddorm.js
+++ routes/adddorm.js
@@ -1,35 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const conn = require('../dbconn');
+const multer = require('multer');
+const path = require('path');
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
 
+const upload = multer({ storage: storage });
 //Routes
 router.get('', (req, res) => {
     res.render('adddorm');
 })
 
-router.get('/process_addorm', function (req, res) {
+
+router.post('/process_addorm', upload.single('image'), (req, res) => {
     let formdata = {
-        name: req.query.name,
-        detail: req.query.detail,
-        address: req.query.address,
-        room: req.query.room,
-        rent: req.query.rent,
-        contact: req.query.contact,
-        other_contact: req.query.other_contact,
-        electric_pay: req.query.electric_pay,
-        water_pay: req.query.water_pay
+        name: req.body.name,
+        detail: req.body.detail,
+        address: req.body.address,
+        room: req.body.room,
+        rent: req.body.rent,
+        contact: req.body.contact,
+        other_contact: req.body.other_contact,
+        electric_pay: req.body.electric_pay,
+        water_pay: req.body.water_pay,
+        image: req.file ? req.file.filename : null
     };
-    console.log(formdata);  
-    let sql = `INSERT INTO users(username, password, email, first_name, last_name, age, address, phone) values ('${formdata.username}', '${formdata.password}', '${formdata.email}', '${formdata.first_name}', '${formdata.last_name}', ${formdata.age}, '${formdata.address}', '${formdata.phone}');`;
+    console.log(formdata);
+    let sql = `INSERT INTO Dormitory(owner_id, name, detail, address, room, status, rent, rating, phone_contact, other_contact, electric_pay, water_pay, dorm_pic) 
+               VALUES (1, '${formdata.name}', '${formdata.detail}', '${formdata.address}', ${formdata.room}, 'Available', '${formdata.rent}', 0.0, '${formdata.contact}', 
+               '${formdata.other_contact}', ${formdata.electric_pay}, ${formdata.water_pay}, '${formdata.image}');`;
     console.log(sql);
     conn.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("a record inserted");
-        res.send(`<script>
-            alert("เพิ่มข้อมูลสำเร็จ");
-            window.location.href = "/";
-        </script>`);
-        
+        console.log("เพิ่มข้อมูลสำเร็จ");
+        res.send(`<script>alert("เพิ่มข้อมูลสำเร็จ"); window.location.href = '/adddorm';</script>`);
     });
 });
 
