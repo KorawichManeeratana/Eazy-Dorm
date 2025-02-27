f6b66356869e6c04893e8270336894781fd713f8
66070224 <66070224@kmitl.ac.th>
2025-02-27 17:17:33 +0700

    เชื่อมหน้าของเองเพชรงับ

--- public/javascripts/selectdorm.js
+++ public/javascripts/selectdorm.js
@@ -14,12 +14,17 @@ function showAllDorm(data) {
     const dorm_container = document.getElementsByClassName("dormcontainer")[0];
     for (dorm of data) {
         dorm_container.insertAdjacentHTML('afterbegin', `<div class="dorm">
-                <img src="/images/${dorm.dorm_pic}" alt="dormimg">
+                <img src="/images/${dorm.dorm_pic}" alt="dorm_img">
                 <div class="describe">
                     <h2>${dorm.name}</h2>
                     <div class="price">${dorm.rent}</div>
-                    <button>รายละเอียด</button>
+                    <button type="button" onclick="openDormInfo(${dorm.dorm_id})">รายละเอียด</button>
                 </div>
             </div>`);
     }
+}
+
+function openDormInfo(id) {
+    window.open(`/dorminfo/${id}`, "_self");
+    return;
 }
