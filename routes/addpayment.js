3aa3b2a1a17e1ec832bde4aadf64a525f1eaa260
insisapim <66070322@kmitl.ac.th>
2025-03-02 15:11:48 +0700

    ..

--- routes/addpayment.js
+++ routes/addpayment.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
+const conn = require('../dbconn');
 
 //Routes
-router.get('', (req, res) => {
-    res.render('addpayment');
-})
+router.get('/:id', async (req, res) => {
+    let dormid = req.params.id;
+    let sql = `
+    SELECT r.room_number, r.status, r.rent, IFNULL(CONCAT(u.first_name, ' ', u.last_name), 'ไม่มีผู้เช่า') AS full 
+    FROM Room r 
+    LEFT JOIN Users u ON r.loger_id = u.user_id 
+    WHERE r.dorm_id = ${dormid};`;
+    let sql2 = `SELECT * FROM Dormitory WHERE dorm_id = ${dormid};`
+    try {
+        const [rows] = await conn.query(sql);
+        const [dormname] = await conn.query(sql2);
+        res.render('addpayment', { data:rows, data2:dormname });
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 module.exports = router;
