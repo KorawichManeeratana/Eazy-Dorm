66d257cad4dde2da7f07e7aa745bfb1f9fa8b628
insisapim <66070322@kmitl.ac.th>
2025-03-03 19:44:23 +0700

    slipmul

--- routes/qrbill.js
+++ routes/qrbill.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const conn = require('../dbconn');
+const multer = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
 
-
+router.post('/gogoqr', upload.single('slip'), async (req, res) => {
+    let formdata ={
+        slip: req.file ? req.file.filename : null
+    };
+    let sql = `INSERT INTO Paymenthistory(room_id, pay_date, pay_img)
+               VALUE (20, 11/11/1111, '${formdata.slip}');`;
+    try {
+        await conn.query(sql);
+        res.send(`<script>alert("เพิ่มข้อมูลสำเร็จ"); window.location.href = '/qrbill/1';</script>`);
+    } catch (err) {
+        console.log(err);
+    }
+});
 //Routes
 router.get('/:qrpid', (req, res) => {
     res.render('qrbill');
