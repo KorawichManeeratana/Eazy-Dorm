ade858746925274827275745dfa544fb7bfb0094
66070224 <66070224@kmitl.ac.th>
2025-03-07 18:48:06 +0700

    แก้นิดหน่อย

--- controller/room/getRoomFloors.js
+++ controller/room/getRoomFloors.js
@@ -3,7 +3,7 @@ const db = require("../../dbconn");
 
 async function getRoomFloors(id) {
   try {
-    query =`SELECT DISTINCT floor FROM Room WHERE dorm_id = ? ORDER BY floor desc;`
+    query =`SELECT DISTINCT floor FROM Room WHERE dorm_id = ? AND loger_id is null ORDER BY floor desc;`
     const result = (await db.query(query, id))[0];
     db.releaseConnection();
     return {
