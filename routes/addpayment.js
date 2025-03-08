d5a40b1b77f1dabd3f5db18525a90a5552722851
insisapim <66070322@kmitl.ac.th>
2025-03-09 00:49:38 +0700

    ข้อมูลรูปภาพ

--- routes/addpayment.js
+++ routes/addpayment.js
@@ -6,37 +6,37 @@ const jwt = require('jsonwebtoken');
 
 //Routes
 router.get('/:id', async (req, res) => {
-    let dormid = req.params.id;
-        const accessToken = req.cookies.access_token;
-        const SECRET_KEY = process.env.ACCESSKEYID;
-        let userIdFromCookie = null;
-      
-        if (accessToken) {
-          try {
-            const decoded = jwt.verify(accessToken, SECRET_KEY);
-            userIdFromCookie = decoded.userID;
-          } catch (error) {
-            console.error("Error decoding JWT:", error);
-            userIdFromCookie = null;
-          }
-        }
-    let sql = `SELECT r.room_id, r.room_number, r.rent, IFNULL(CONCAT(u.first_name, ' ', u.last_name), 'ไม่มีผู้เช่า') AS full 
+  let dormid = req.params.id;
+  const accessToken = req.cookies.access_token;
+  const SECRET_KEY = process.env.ACCESSKEYID;
+  let userIdFromCookie = null;
+
+  if (accessToken) {
+    try {
+      const decoded = jwt.verify(accessToken, SECRET_KEY);
+      userIdFromCookie = decoded.userID;
+    } catch (error) {
+      console.error("Error decoding JWT:", error);
+      userIdFromCookie = null;
+    }
+  }
+  let sql = `SELECT r.room_id, r.room_number, r.rent, IFNULL(CONCAT(u.first_name, ' ', u.last_name), 'ไม่มีผู้เช่า') AS full 
               FROM Room r 
               LEFT JOIN Users u ON r.loger_id = u.user_id 
               WHERE r.dorm_id = ${dormid};`;
-              let sql2 = `SELECT * FROM Dormitory WHERE dorm_id = ${dormid};`
-    try {
-        const [rows] = await conn.query(sql);
-        const [dormname] = await conn.query(sql2);
-        if (dormname[0].owner_id != userIdFromCookie){
-            return res.redirect('/');
-        }
-        res.render('addpayment', { data:rows, data2:dormname });
-
-        conn.releaseConnection();
-    } catch (err) {
-        console.log(err);
+  let sql2 = `SELECT * FROM Dormitory WHERE dorm_id = ${dormid};`
+  try {
+    const [rows] = await conn.query(sql);
+    const [dormname] = await conn.query(sql2);
+    if (dormname[0].owner_id != userIdFromCookie) {
+      return res.redirect('/');
     }
+    res.render('addpayment', { data: rows, data2: dormname });
+
+    conn.releaseConnection();
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
